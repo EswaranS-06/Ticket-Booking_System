c089df802ff340365c21d676ab335046edbf90d2
EswaranS-06 <eswaransk06@gmail.com>
2025-07-20 13:52:20 +0530

    almost done

--- ticket_reserverstion_system.py
+++ ticket_reserverstion_system.py
@@ -11,9 +11,8 @@ class train():
         for section in self.sections:
             #for i in range(4):
             print(section)
-            section.display()
+            #section.display()
             
-        
     def TotalReservedSeats(self, seat_type=None):
         max_seats = 0
         for section in self.sections:
@@ -27,30 +26,46 @@ class train():
             else:
                 return i
         else:
-            return #worst case <--------------------------------
+            return None #worst case <--------------------------------
         
     def booking(self):
         rev_type = int(input("1. Takkal\n2. Reservation\nEnter (1,2): "))
-        if rev_type == 1 and self.TotalReservedSeats() < 50:
-            self.takkalBook()
+        if rev_type == 1:
+            if self.TotalReservedSeats() > 50:
+                self.booking_type(0)
+            else :
+                print("Sorry Takkal is Not open till now")
+            
+        elif rev_type == 2:
+            self.booking_type(1)
+            
         else:
-            pass
-        
-    def takkalBook(self):
-        num_seats = int(input("How many tickets [MAX:20]: "))
+            print("Invalid Input Try To Use Only the (1 or 2)")
+                     
+    def booking_type(self, is_takkal):
         seat_type = int(input("1. Lower\n2. Middle\n3. Upper\nEnter (1,2,3): "))-1
+        if not seat_type in range(0,3):
+            print("Invalid selection try to use only the above options")
+            return
+        num_seats = int(input("How many tickets [MAX:20]: "))
+        if num_seats > 20:
+            print("Sorry the Maximum limit is 20")
+            return
         info_type = int(input("1. Book every ticket in your\n2. Give Names for every Ticket\nOnce selected process is not Revertable\nEnter your choise (1,2): "))
+        
         if info_type == 1:
             c = customer()
             self.customers.append(c.setInfo())
-            temp = self.availableSection(num_seats, seat_type, 0)
+            temp = self.availableSection(num_seats, seat_type, is_takkal)
+            if temp == None:
+                print("Sorry, There is a Shortage of seats currently")
             for i in range(num_seats):
                 self.sections[temp].book(seat_type, self.customers[-1])
                 
             else:
                 print(f"Succussefullly Booked {num_seats} seats\nin the Name of {self.customers[-1][0]}")
         elif info_type == 2:
-            temp = self.availableSection(num_seats, seat_type, 0)
+            temp = self.availableSection(num_seats, seat_type, is_takkal)
             for i in range(num_seats):
                 c = customer()
                 self.customers.append(c.setInfo())
@@ -58,9 +73,6 @@ class train():
                 
             else:
                 print(f"Succussefullly Booked {num_seats} seats\nin the Name of {self.customers[-1][0]}")
-                
-    def reserveBook(self):
-        pass
 
 class sleeper():
     def __init__(self, is_takkal, num_seats):
@@ -105,8 +117,8 @@ class customer():
             age = 0
         self.info.append(name)
         self.info.append(age)
-        # self.info.append("Rex")
-        # self.info.append(20)
+        # self.info.append("e")
+        # self.info.append(0)
         return self.info
     
     def getInfo(self):
@@ -114,9 +126,21 @@ class customer():
     
     
 t = train()
-while True:        
- 
-    t.booking()
-    i = input()
-    if i == "o":
-        t.display()
+def main():
+    dec = input("Are you looking to Book Ticket? (Yes[y]/No[n])").strip().lower()
+    if "n" in dec:
+        print("Your Welcome!!!")
+        return
+    elif "a" == dec:
+        return #<------ add train adding function
+    while True:        
+        print("""
+            Hello your Welcome to the OUR
+                \033[;34mTicket Booking Service \033[0m""")
+        
+        
+        i = input()
+        
+        
+if __name__ == main():
+    main()
